1991ae5d5a067d7286b9bf5aa560ca81c2c2603f
valflrt <vfleurit@gmail.com>
2022-07-11 08:09:19 +0200

    Edit test

--- test/Collection.test.ts
+++ test/Collection.test.ts
@@ -1,22 +1,22 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
 
-import { Collection } from "../src/Collection";
+import { EnhancedMap } from "../src/EnhancedMap";
 
 describe("Collection tests", () => {
   describe("Collection.set()", () => {
     it("should set an entry properly", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("rick", "https://youtu.be/dQw4w9WgXcQ");
-      expect(collection.rawEntries).to.deep.equal([
-        { key: "rick", value: "https://youtu.be/dQw4w9WgXcQ" },
-      ]);
+      expect(collection).to.deep.equal(
+        new Map<string, any>().set("rick", "https://youtu.be/dQw4w9WgXcQ")
+      );
     });
   });
 
   describe("Collection.get()", () => {
     it("should get an entry properly", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("rick", "https://youtu.be/dQw4w9WgXcQ");
       expect(collection.get("rick")).to.equal("https://youtu.be/dQw4w9WgXcQ");
     });
@@ -24,25 +24,25 @@ describe("Collection tests", () => {
 
   describe("Collection.has()", () => {
     it("should return true if the entry exists", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       expect(collection.has("greetings")).to.be.true;
     });
     it("should return false if the entry doesn't exist", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       expect(collection.has("greetings")).to.be.false;
     });
   });
 
   describe("Collection.hasAll()", () => {
     it("should return true if all specified entries exist", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       collection.set("rick", "https://youtu.be/dQw4w9WgXcQ");
       expect(collection.hasAll("greetings", "rick")).to.be.true;
     });
     it("should return false if one or more of the specified entries doesn't exist", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       expect(collection.hasAll("greetings", "rick")).to.be.false;
       expect(collection.hasAll("greetings", "rick", "nothing")).to.be.false;
@@ -51,12 +51,12 @@ describe("Collection tests", () => {
 
   describe("Collection.hasAny()", () => {
     it("should return true if one or more of the specified entries exists", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       expect(collection.hasAny("greetings", "rick", "nothing")).to.be.true;
     });
     it("should return false if none of the specified entries exist", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       expect(collection.hasAny("rick", "nothing")).to.be.false;
     });
@@ -64,7 +64,7 @@ describe("Collection tests", () => {
 
   describe("Collection.remove()", () => {
     it("should remove properly the entries that match the filter", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       collection.set("one", 1);
       collection.set("rick", "https://youtu.be/dQw4w9WgXcQ");
@@ -79,7 +79,7 @@ describe("Collection tests", () => {
 
   describe("Collection.removeKeys()", () => {
     it("should properly remove entries with the specified keys", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       collection.set("one", 1);
       collection.set("rick", "https://youtu.be/dQw4w9WgXcQ");
@@ -92,9 +92,24 @@ describe("Collection tests", () => {
     });
   });
 
+  describe("Collection.some()", () => {
+    it("should check if one of the Collection entries matches the filter", () => {
+      let collection = new EnhancedMap();
+      collection.set("greetings", ["hello", "hi", "goodbye"]);
+      collection.set("one", 1);
+      collection.set("rick", "https://youtu.be/dQw4w9WgXcQ");
+      collection.set("array", [1, 2, 3]);
+
+      expect(collection.some(({ key }) => key === "one")).to.be.true;
+      expect(
+        collection.some(({ value }) => value === "https://youtu.be/dQw4w9WgXcQ")
+      ).to.be.true;
+    });
+  });
+
   describe("Collection.forEach()", () => {
     it("should execute a function for each entry in the collection", () => {
-      let collection = new Collection();
+      let collection = new EnhancedMap();
       collection.set("greetings", ["hello", "hi", "goodbye"]);
       collection.set("one", 1);
       collection.set("rick", "https://youtu.be/dQw4w9WgXcQ");
@@ -103,7 +118,7 @@ describe("Collection tests", () => {
       let keys: string[] = [];
       let values: any[] = [];
 
-      collection.forEach((e) => {
+      collection.each((e) => {
         keys.push(e.key);
         values.push(e.value);
       });
@@ -120,24 +135,25 @@ describe("Collection tests", () => {
 
   describe("Collection.map()", () => {
     it("should execute a function for each item that maps the original Collection into another", () => {
-      let collection = new Collection<number>();
+      let collection = new EnhancedMap<number>();
       collection.set("one", 1);
       collection.set("two", 2);
       collection.set("three", 3);
 
       let newCollection = collection.map((e) => [e.key, e.key]);
 
-      expect(newCollection.rawEntries).to.deep.equal([
-        { key: "one", value: "one" },
-        { key: "two", value: "two" },
-        { key: "three", value: "three" },
-      ]);
+      expect(newCollection).to.deep.equal(
+        new Map<string, any>()
+          .set("one", "one")
+          .set("two", "two")
+          .set("three", "three")
+      );
     });
   });
 
   describe("Collection.find()", () => {
     it("should find an entry that matches the specified filter", () => {
-      let collection = new Collection<number>();
+      let collection = new EnhancedMap<number>();
       collection.set("zero point five", 0.5);
       collection.set("one", 1);
       collection.set("one point five", 1.5);
@@ -148,7 +164,7 @@ describe("Collection tests", () => {
       expect(found?.value).to.equal(1);
     });
     it("should return null if no entry matches the specified filter", () => {
-      let collection = new Collection<number>();
+      let collection = new EnhancedMap<number>();
       collection.set("one", 1);
       collection.set("two", 2);
       collection.set("three", 3);
@@ -161,7 +177,7 @@ describe("Collection tests", () => {
 
   describe("Collection.findAll()", () => {
     it("should find all entries that match the specified filter", () => {
-      let collection = new Collection<number>();
+      let collection = new EnhancedMap<number>();
       collection.set("zero point five", 0.5);
       collection.set("one", 1);
       collection.set("one point five", 1.5);
@@ -178,7 +194,7 @@ describe("Collection tests", () => {
       ).to.be.true;
     });
     it("should return null if no entry matches the specified filter", () => {
-      let collection = new Collection<number | string>();
+      let collection = new EnhancedMap<number | string>();
       collection.set("one", 1);
       collection.set("two", 2);
       collection.set("three", 3);
@@ -191,7 +207,7 @@ describe("Collection tests", () => {
 
   describe("Collection.filter()", () => {
     it("should execute a function for each item that filters the original Collection and makes another", () => {
-      let collection = new Collection<number>();
+      let collection = new EnhancedMap<number>();
       collection.set("zero point five", 0.5);
       collection.set("one", 1);
       collection.set("one point five", 1.5);
@@ -199,10 +215,9 @@ describe("Collection tests", () => {
 
       let newCollection = collection.filter((e) => Number.isInteger(e.value));
 
-      expect(newCollection.rawEntries).to.deep.equal([
-        { key: "one", value: 1 },
-        { key: "two", value: 2 },
-      ]);
+      expect(newCollection).to.deep.equal(
+        new Map<string, number>().set("one", 1).set("two", 2)
+      );
     });
   });
 });
